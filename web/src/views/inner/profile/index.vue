9634da2e5d6c166b5bea971463365ebce59d4678
Aditya Choudhary <adityachoudhary.jsr@gmail.com>
2019-03-10 11:57:46 +0530

    routes of inner folder added

---
+++ web/src/views/inner/profile/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        profile
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'profile'
+}
+</script>
--- web/src/views/inner/routes.js
+++ web/src/views/inner/routes.js
@@ -1,9 +1,27 @@
 var home = () => import('./home/')
+var profile = () => import('./profile/')
+var bookmarks = () => import('./bookmarks/')
+var researchPaper = () => import('./researchPaper/')
 
 export default [
   {
     path: '/',
     name: 'home',
     component: home
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: profile
+  },
+  {
+    path: '/bookmarks',
+    name: 'bookmarks',
+    component: bookmarks
+  },
+  {
+    path: '/paper/:pid',
+    name: 'researchPaper',
+    component: researchPaper
   }
 ]
