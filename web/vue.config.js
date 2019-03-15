4dc10bb6d58082f0338052f4efaf4b01a3543993
Mukul Khanna <mukul18khanna@gmail.com>
2019-03-15 17:01:07 +0530

    Config

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/researchflix/'
-      : '/'
+    baseUrl: '/researchflix/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/researchflix/'
+    //   : '/'
   }
